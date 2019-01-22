e2d8439860135b2b8a181670a1629f0df123f0de
codesworth <codezworth@gmail.com>
2019-01-22 11:19:06 +0000

    Initial Commit

---
+++ ProgressKit/ProgressKit.h
@@ -0,0 +1,19 @@
+//
+//  ProgressKit.h
+//  ProgressKit
+//
+//  Created by Mensah Shadrach on 22/01/2019.
+//  Copyright © 2019 Mensah Shadrach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProgressKit.
+FOUNDATION_EXPORT double ProgressKitVersionNumber;
+
+//! Project version string for ProgressKit.
+FOUNDATION_EXPORT const unsigned char ProgressKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressKit/PublicHeader.h>
+
+
