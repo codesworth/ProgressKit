fa1b4fa1daeb8d66e29bcb29c2d47ff3369df8af
codesworth <codezworth@gmail.com>
2019-01-23 15:29:14 +0000

    Fixed Twitterstyle AlertViews to fit unlimited number of Options

---
+++ ProgressKit/Sources/Extensions/UIKIt+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIKIt+Extensions.swift
+//  ProgressKit
+//
+//  Created by Mensah Shadrach on 23/01/2019.
+//  Copyright © 2019 Mensah Shadrach. All rights reserved.
+//
+
+import Foundation
