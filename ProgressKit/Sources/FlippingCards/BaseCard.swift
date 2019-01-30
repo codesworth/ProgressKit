cb74b4b19bb7129a3e14fc5e5c270673b20c9eae
codesworth <codezworth@gmail.com>
2019-01-30 22:51:49 +0000

    Bgun Flipping Card View

---
+++ ProgressKit/Sources/FlippingCards/BaseCard.swift
@@ -0,0 +1,28 @@
+
+//
+//  BaseCard.swift
+//  ProgressKit
+//
+//  Created by Mensah Shadrach on 30/01/2019.
+//  Copyright © 2019 Mensah Shadrach. All rights reserved.
+//
+
+import UIKit
+
+class BaseCard: UIView {
+
+    override init(frame:CGRect){
+        super.init(frame:frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+
+}
