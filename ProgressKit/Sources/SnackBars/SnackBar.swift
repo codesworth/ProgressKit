dd9780f9789cb0f8dab8f7cb0adf909726a7763f
codesworth <codezworth@gmail.com>
2019-02-01 23:50:14 +0000

    First Gen Snackbar Created

--- ProgressKit/Sources/SnackBars/SnackBar.swift
+++ ProgressKit/Sources/SnackBars/SnackBar.swift
@@ -9,13 +9,26 @@
 import UIKit
 
 class SnackBar: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    class func makeSnackIn(_ parent:UIView, text:String, color:UIColor = .seafoamBlue){
+        let snackbar = SnackBar(frame: CGRect(x: 0, y: parent.frame.height, width: parent.frame.width, height: 40))
+        snackbar.backgroundColor = color
+        parent.addSubview(snackbar)
+        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.2, options: .curveEaseInOut, animations: {
+            snackbar.frame.origin.y -= 40
+        })
+        UIView.animate(withDuration: 0.5, delay: 4, options: .curveEaseOut, animations: {
+            snackbar.frame.origin.y += 50
+        }) { (success) in
+            snackbar.removeFromSuperview()
+        }
+    }
 }
