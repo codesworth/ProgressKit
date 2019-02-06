fcdbc30b5fc2b0a094a165684bf356c52421efd8
Codesworth <codezworth@gmail.com>
2019-02-06 23:49:59 +0000

    Adding Ainmation Effects

--- ProgressKit/Sources/PKAlertView.swift
+++ ProgressKit/Sources/PKAlertView.swift
@@ -26,7 +26,7 @@ public class PKAlertView:UIView{
         return view
     }()
     
-    override init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
         
     }
@@ -34,17 +34,17 @@ public class PKAlertView:UIView{
     public override func layoutSubviews() {
         super.layoutSubviews()
         overlay.frame = nativeFrame
-        alertView.center = center
         alertView.frame.size = CGSize(width: nativeFrame.width * 0.7, height: 200)
-        alertView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        alertView.center = center
+        alertView.transform.scaledBy(x: 0.1, y: 0.1)
         addSubview(overlay)
         addSubview(alertView)
     }
     
     public override func didMoveToWindow() {
         super.didMoveToWindow()
-        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.2, options: .curveEaseInOut, animations: {
-            self.alertView.transform = CGAffineTransform.identity
+        UIView.animate(withDuration: 1, delay: 2, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.2, options: .curveEaseInOut, animations: {
+            self.alertView.transform.scaledBy(x: 1, y: 1)
         })
     }
     
--- ProgressKitExamples/ProgressKitExamples/ViewController.swift
+++ ProgressKitExamples/ProgressKitExamples/ViewController.swift
@@ -18,15 +18,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
-        array = [arb,zp]
-        print("The first iterations are: \(array.debugDescription)")
-        
-        arb.itemcount = 1000
-        print("The second iterations are: \(array.debugDescription)")
-        
-        array.forEach { (item) in
-            print("This is the count: \(item.itemName) and \(item.itemcount)")
-        }
+//        array = [arb,zp]
+//        print("The first iterations are: \(array.debugDescription)")
+//
+//        arb.itemcount = 1000
+//        print("The second iterations are: \(array.debugDescription)")
+//
+//        array.forEach { (item) in
+           // print("This is the count: \(item.itemName) and \(item.itemcount)")
+       // }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -39,20 +39,21 @@ class ViewController: UIViewController {
 //
 //        let loader = ActivityLoaderView(frame: view.frame, offset: 0, text: "Heyaaa")
 //        view.addSubview(loader)
-        let button = UIButton(frame: CGRect(origin: .zero, size: CGSize(width: 200, height: 50)))
-        button.backgroundColor = UIColor.brown
-        button.setTitleColor(.white, for: .normal)
-        button.setTitle("Press Me", for: .normal)
-        button.addTarget(self, action: #selector(pressMe), for: .touchUpInside)
-        view.addSubview(button)
-        button.center = view.center
+//        let button = UIButton(frame: CGRect(origin: .zero, size: CGSize(width: 200, height: 50)))
+//        button.backgroundColor = UIColor.brown
+//        button.setTitleColor(.white, for: .normal)
+//        button.setTitle("Press Me", for: .normal)
+//        button.addTarget(self, action: #selector(pressMe), for: .touchUpInside)
+//        view.addSubview(button)
+//        button.center = view.center
 //
-        let options = [("Pay 10$",{print("I Paid holy <Molly")},UIImage(named: "ic")),("Pay 1000$",{print("I Paid holy <Moses")},UIImage(named: "ic"))]
-        alert = TWAlertView(options: options)
+        //let options = [("Pay 10$",{print("I Paid holy <Molly")},UIImage(named: "ic")),("Pay 1000$",{print("I Paid holy <Moses")},UIImage(named: "ic"))]
+ //       alert = TWAlertView(options: options)
 //        var flip = FlippingCards()
 //        view.addSubview(flip)
-        SnackBar.makeSnackIn(view, text: "Hey There Boys Im snacky")
-        
+        //SnackBar.makeSnackIn(view, text: "Hey There Boys Im snacky")
+        let v = PKAlertView(frame:view.frame)
+        view.addSubview(v)
         
     }
     
