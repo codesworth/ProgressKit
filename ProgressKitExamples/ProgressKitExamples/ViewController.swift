a7c6b3fbbc47b742434ce69f723c73641a776a94
Codesworth <codezworth@gmail.com>
2019-02-08 23:30:16 +0000

    Examples Clean Up

--- ProgressKitExamples/ProgressKitExamples/ViewController.swift
+++ ProgressKitExamples/ProgressKitExamples/ViewController.swift
@@ -18,43 +18,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
-//        array = [arb,zp]
-//        print("The first iterations are: \(array.debugDescription)")
-//
-//        arb.itemcount = 1000
-//        print("The second iterations are: \(array.debugDescription)")
-//
-//        array.forEach { (item) in
-           // print("This is the count: \(item.itemName) and \(item.itemcount)")
-       // }
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-//        let progress = FullScaffoldView(frame: view.frame)
-//        progress.showProgress(with: 20)
-//        view.addSubview(progress)
-//
-//        let loader = ActivityLoaderView(frame: view.frame, offset: 0, text: "Heyaaa")
-//        view.addSubview(loader)
-//        let button = UIButton(frame: CGRect(origin: .zero, size: CGSize(width: 200, height: 50)))
-//        button.backgroundColor = UIColor.brown
-//        button.setTitleColor(.white, for: .normal)
-//        button.setTitle("Press Me", for: .normal)
-//        button.addTarget(self, action: #selector(pressMe), for: .touchUpInside)
-//        view.addSubview(button)
-//        button.center = view.center
-//
-        //let options = [("Pay 10$",{print("I Paid holy <Molly")},UIImage(named: "ic")),("Pay 1000$",{print("I Paid holy <Moses")},UIImage(named: "ic"))]
- //       alert = TWAlertView(options: options)
-//        var flip = FlippingCards()
-//        view.addSubview(flip)
-        //SnackBar.makeSnackIn(view, text: "Hey There Boys Im snacky")
+
+        
+    }
+    
+    
+    func snack(){
+        SnackBar.makeSnackIn(view, text: "Hey There Boys Im snacky")
+    }
+    
+    func PKAlert(){
         let v = PKAlertView(frame:view.frame)
         view.addSubview(v)
-        
+    }
+    
+    
+    func loaderActivity(){
+        let loader = ActivityLoaderView(frame: view.frame, offset: 0, text: "Heyaaa")
+        view.addSubview(loader)
+    }
+    
+    func scafolding(){
+        let progress = FullScaffoldView(frame: view.frame)
+        progress.showProgress(with: 20)
+        view.addSubview(progress)
+    }
+    
+    func twitterAlert(){
+    let button = UIButton(frame: CGRect(origin: .zero, size: CGSize(width: 200, height: 50)))
+    button.backgroundColor = UIColor.brown
+    button.setTitleColor(.white, for: .normal)
+    button.setTitle("Press Me", for: .normal)
+    button.addTarget(self, action: #selector(pressMe), for: .touchUpInside)
+    view.addSubview(button)
+    button.center = view.center
+    let options = [("Pay 10$",{print("I Paid holy <Molly")},UIImage(named: "ic")),("Pay 1000$",{print("I Paid holy <Moses")},UIImage(named: "ic"))]
+           alert = TWAlertView(options: options)
+    }
+    
+    func flipAcard(){
+        let flip = FlippingCards()
+        view.addSubview(flip)
     }
     
     @objc func pressMe(){
